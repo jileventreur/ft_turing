48cdf9a960dfed4a2d4c09ec21b1096856d0eb25
Zleub <debray.arnaud@gmail.com>
2017-08-13 21:41:11 +0200

    print json fn

--- srcs/main.ml
+++ srcs/main.ml
@@ -1,10 +1,34 @@
+open Json
 open Opt
+
+let print json =
+	print_endline ("name: " ^ json.name) ;
+	print_string "alphabet:" ;
+	let print_alphablet c =
+		print_string " ";
+		print_char c
+	in CharSet.iter print_alphablet json.alphabet ;
+	print_endline "" ;
+	print_string ("blank: ");
+	print_char json.blank ;
+	print_endline "";
+	print_string "states:" ;
+	let print_states c =
+		print_string " ";
+		print_string c
+	in StringSet.iter print_states json.states ;
+	print_endline "" ;
+	print_string "finals:" ;
+	StringSet.iter print_states json.finals ;
+	print_endline "" ;
+	print_endline ("initial: " ^ json.initial) ;
+	print_endline "transitions:";
+	let _print s = print_endline ("\t" ^ (fst s)) in
+	List.iter _print json.transitions
 
 let () =
 	let argv = getopt Sys.argv in
-	print_endline ("json : " ^ argv.jsonfile) ;
-	print_endline ("input : " ^ argv.input) ;
-	ignore (Json.extract "json/unary_Sub.json")
+	print (extract argv.jsonfile)
 
 (*
 let () =
