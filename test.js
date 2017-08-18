66cb3e4820f24c4f7cbee835485f8c1ef9fc3e2d
Zleub <debray.arnaud@gmail.com>
2017-08-18 02:19:57 +0200

    fix alphabets conflicts

--- test.js
+++ test.js
@@ -7,7 +7,7 @@
 //  sdddddddddddddddddddddddds   @Last modified by: adebray
 //  sdddddddddddddddddddddddds
 //  :ddddddddddhyyddddddddddd:   @Created: 2017-08-17T23:47:18+02:00
-//   odddddddd/`:-`sdddddddds    @Modified: 2017-08-18T00:36:51+02:00
+//   odddddddd/`:-`sdddddddds    @Modified: 2017-08-18T02:18:51+02:00
 //    +ddddddh`+dh +dddddddo
 //     -sdddddh///sdddddds-
 //       .+ydddddddddhs/.
@@ -21,25 +21,56 @@ const log = o => console.log(require('util').inspect(o, { colors: true, depth: n
 
 if (process.argv.length != 4) {
 	console.log('usage: ./test.js jsonfile input')
+	process.exit(-1)
 }
 
 let out = ""
-let refs = []
+let refs = {}
+
+refs.__proto__._ = []
+for (var i = 48; i < 58; i++)
+	refs.__proto__._.push( String.fromCharCode(i) )
+for (var i = 65; i < 91; i++)
+	refs.__proto__._.push( String.fromCharCode(i) )
+for (var i = 97; i < 122; i++)
+	refs.__proto__._.push( String.fromCharCode(i) )
+
+refs.__proto__.push = function (s) {
+	let e = _.shift()
+	while (Object.keys(this).some(_ => _ == e))
+		e = _.shift()
+	this[e] = s
+}
+refs.__proto__.indexOf = function (s) {
+	return Object.keys(this).reduce( (p, k) => {
+		if (this[k] == s)
+			return k
+		else return p
+	}, null)
+}
 let data = require(process.argv[2])
 
 Object.keys(data).forEach( e => {
-	out += "|"
+	out += out[out.length - 1] == '|' ? "" : "|"
 	let v = data[e]
-	// log(v)
-	if (v instanceof Array) {
+	if ( e == 'name') {}
+	else if (v instanceof Array) {
 		if (e == 'states') {
 			out += v.reduce( (p, e) => {
+				// if (refs[e])
 				refs.push(e)
+				console.log(e)
 				return p += refs.indexOf(e)
 			}, "")
 		}
 		else if (e == 'finals') {
-			out += v.reduce( (p, e) => p += refs.indexOf(e) , "")
+			out += v.reduce( (p, e) => p += refs.indexOf(e), "")
+		}
+		else if (e == 'alphabet') {
+			out += v.reduce( (p, e) => {
+				refs[e] = true
+				return p += e
+			}, "")
 		}
 		else {
 			out += v.reduce( (p, e) => p += e, "")
@@ -58,3 +89,16 @@ Object.keys(data).forEach( e => {
 })
 out += '|' + process.argv[3] + '|'
 console.log(out)
+console.log("-----------")
+
+let alphabet = () => Object.keys( Array.from(out).reduce( (p,e) => {
+	if (!p[e])
+		p[e] = true
+	return p
+}, {} ) )
+
+log({
+	'name': 'sim_' + process.argv[2].match(/\/([^/]*)$/)[1],
+	'alphabet': alphabet(),
+	'blank': ' '
+})
