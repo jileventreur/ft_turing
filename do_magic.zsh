387cee9d5a8489e8edcdbfbdc36c5783d57dda27
Zleub <debray.arnaud@gmail.com>
2017-08-27 02:03:39 +0200

    ongoing work merge

--- do_magic.zsh
+++ do_magic.zsh
@@ -2,8 +2,30 @@
 # @Date:   2017-08-26T01:32:00+02:00
 # @Email:  adebray@student.42.fr
 # @Last modified by:   adebray
-# @Last modified time: 2017-08-27T00:57:57+02:00
+# @Last modified time: 2017-08-27T02:00:38+02:00
 
-ocamlfind ocamlc -thread -package str,yojson -linkpkg compiler.ml -o ft_compiler \
-&& ./ft_compiler $1 $2 \
-&& ./ft_turing $3 $4 sim_${1:t} `cat input_${1:t}`
+ocamlfind ocamlc -thread -package str,yojson -linkpkg ft_compiler.ml -o ft_compiler &&
+./ft_compiler $1 $2 &&
+ocamlfind ocamlopt -thread -package core,yojson -I ./compiler -c compiler/compiler.ml &&
+ocamlfind ocamlopt -linkpkg -thread -package core,yojson -o compiler/compile ./compiler/compiler.cmx &&
+rm -rf machines/complete_turing.json && ./compiler/compile &&
+mv complete_turing.json machines
+
+node <<EOF
+let check = require("./sim_ispair.json")
+let exec = require("./machines/complete_turing.json")
+
+check.transitions["ERASE"] = check.transitions["ERASE"].map( e => {
+	if (e.to_state == "TEST")
+		e.to_state = exec.initial
+	return e
+})
+
+check.states = check.states.concat(exec.states).filter(function(item, pos, a) {
+    return a.indexOf(item) == pos;
+})
+check.transitions = Object.assign(check.transitions, exec.transitions)
+
+require('fs').writeFileSync('test.json', JSON.stringify(check, null, "  "))
+
+EOF
--- script_compile.sh
+++ script_compile.sh
@@ -1,3 +1,9 @@
+# @Author: Debray Arnaud <adebray>
+# @Date:   2017-08-27T00:50:57+02:00
+# @Email:  adebray@student.42.fr
+# @Last modified by:   adebray
+# @Last modified time: 2017-08-27T01:03:53+02:00
+
 ocamlfind ocamlopt -thread -package core,yojson -I ./compiler -c compiler/compiler.ml &&
 ocamlfind ocamlopt -linkpkg -thread -package core,yojson -o compiler/compile ./compiler/compiler.cmx &&
 rm -rf machines/complete_turing.json && ./compiler/compile &&
